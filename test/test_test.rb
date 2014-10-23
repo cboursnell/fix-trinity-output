815eb7bf156b9a6d00d6a5b163dc383a038ca516
Chris Boursnell <cboursnell@gmail.com>
2014-10-23 16:07:59 +0100

    improve tests

--- test/test_test.rb
+++ test/test_test.rb
@@ -9,23 +9,28 @@ class TestFixer < Test::Unit::TestCase
 
     setup do
       @fixer = Fixer.new
+      @tmpdir = Dir.mktmpdir
     end
 
     teardown do
+      cmd = "rm -rf #{@tmpdir}"
+      `#{cmd}`
     end
 
     should 'fix pair of fastq files' do
       left = File.join(File.dirname(__FILE__), 'left.fq')
       right = File.join(File.dirname(__FILE__), 'right.fq')
-      tmpdir = Dir.mktmpdir
-      prefix = "#{tmpdir}/test"
+      prefix = "#{@tmpdir}/test"
       @fixer.run left, right, prefix
-      assert File.exist?("#{tmpdir}/test-fixed.1.fastq")
-      assert File.exist?("#{tmpdir}/test-fixed.2.fastq")
-      assert File.exist?("#{tmpdir}/test-fixed.single.fastq")
-      assert_equal 48789, File.size("#{tmpdir}/test-fixed.1.fastq")
-      assert_equal 50035, File.size("#{tmpdir}/test-fixed.2.fastq")
-      assert_equal 762, File.size("#{tmpdir}/test-fixed.single.fastq")
+      assert File.exist?("#{@tmpdir}/test-fixed.1.fastq")
+      assert File.exist?("#{@tmpdir}/test-fixed.2.fastq")
+      assert File.exist?("#{@tmpdir}/test-fixed.single.fastq")
+      lines = `wc -l #{@tmpdir}/test-fixed.1.fastq`
+      assert_equal 876, lines.split(/\s+/).first.to_i
+      lines = `wc -l #{@tmpdir}/test-fixed.2.fastq`
+      assert_equal 876, lines.split(/\s+/).first.to_i
+      assert_equal 762, File.size("#{@tmpdir}/test-fixed.single.fastq")
+
     end
 
   end
